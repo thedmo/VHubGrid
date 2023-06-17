64f0a004ad2a3685daa6059d5ef6ff2abbd2cb3f
Minjaben <benjaminehauser@gmail.com>
2023-06-17 13:21:40 +0200

    adds testproject for wrapper

---
+++ WrapperTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace WrapperTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RouterApiWrapper;
+    
+    
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+                   
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
