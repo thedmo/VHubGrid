f8de1d952e32e809a53bcbe88c8e48e51035bcbe
Minjaben <benjaminehauser@gmail.com>
2023-06-26 12:37:28 +0200

    Adds Function to chekc if simulator is running

--- WrapperTests/UnitTest1.cs
+++ WrapperTests/UnitTest1.cs
@@ -2,8 +2,8 @@ namespace WrapperTests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using RouterApiWrapper;
-    
-    
+    using System.Diagnostics;
+
     [TestClass]
     public class UnitTest1
     {
@@ -14,7 +14,28 @@ namespace WrapperTests
             int result = routerApiWrapper.AddRouter("127.0.0.1");
 
             Assert.AreEqual(0, result);
+        }
 
+        // Check if Simulator Process is running. not a Testmethod
+        public static bool CheckSimulatorProcessRunning()
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process process in processlist)
+            {
+                if (String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    continue;
+                }
+
+                if (!(process.ProcessName == "BmdVideoHub"))
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
+                return true;
+            }
+            return false;
         }
     }
 }
